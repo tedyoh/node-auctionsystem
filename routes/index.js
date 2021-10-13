a5126c81f9a2b8611728f060cf2e47b780c041d9
tedyoh <87564911+tedyoh@users.noreply.github.com>
2021-10-13 23:14:54 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -56,7 +56,7 @@ const upload = multer({
             cb(null, path.basename(file.originalname, ext)+new Date().valueOf()+ext);
         },
     }),
-    limits : {fileSize : 5 * 1024 * 1024},
+    limits : {fileSize : 20 * 1024 * 1024},
 });
 
 router.post('/good', isLoggedIn, upload.single('img'), async(req, res, next)=> {
